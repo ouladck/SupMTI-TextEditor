858260486440da65317539cd16e6fac3ed80aeba
Karim <herr.linux88@gmail.com>
2019-05-27 15:28:49 +0000

    INIT website

--- dialogs/__init__.py
+++
@@ -1,6 +0,0 @@
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-from gi.repository import Gio
-from gi.repository import GdkPixbuf
-from gettext import gettext as g
--- widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-import gi
-gi.require_version('Gtk', '3.0')
-gi.require_version('GtkSource', '4')
-from gi.repository import Gtk
-from gi.repository import Gio
-from gi.repository import Pango
-from gi.repository import GdkPixbuf
-from gettext import gettext as g
-from gi.repository import GtkSource
